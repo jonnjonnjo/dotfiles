e3d7a22e0b650aec81fb00fac8e24f05f4eb6a23
jonnjonnjo <zhenagajo234@gmail.com>
2025-12-23 22:21:37 +0700

    feat: add nvim config. Will find previous plugin later

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n","-","<CMD>Oil<CR>",{ desc = "Open parent directory" })
+
+
---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,14 @@
+return {
+	'akinsho/bufferline.nvim', 
+	version = "*", 
+	dependencies = 'nvim-tree/nvim-web-devicons',
+	opts = {
+		options = {
+		    indicator = {
+			icon = '▎', -- this should be omitted if indicator style is not 'icon'
+			style =  'underline' ,
+		    },
+
+		}
+	}
+}
