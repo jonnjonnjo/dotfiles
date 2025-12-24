1e2c8ca744129cebc5a0e77cf71d2ce2048e5290
jonnjonnjo <zhenagajo234@gmail.com>
2025-12-24 21:38:55 +0700

    feat: add binding for changing wallpapers

--- .config/hypr/scripts/random-wallpaper.sh
+++ .config/hypr/scripts/random-wallpaper.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 ONE_RANDOM_WALLPAPER=$(find ~/Wallpapers -type f \( -name "*.jpg" -o -name "*.png" \) | shuf -n 1)
-awww img "$ONE_RANDOM_WALLPAPER"
-
+awww img "$ONE_RANDOM_WALLPAPER" --transition-step 90 
