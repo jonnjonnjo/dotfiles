27b7eb3db30423bd9593bbeeb1537ef72a9e75b4
jonnjonnjo <zhenagajo234@gmail.com>
2025-12-28 19:21:07 +0700

    feat: add keymap for markdownpreview

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -6,4 +6,8 @@ return {
     vim.g.mkdp_filetypes = { "markdown" }
   end,
   ft = { "markdown" },
+  keys = {
+    { "<leader>mp", "<cmd>MarkdownPreview<cr>", desc = "Markdown Preview" },
+    { "<leader>ms", "<cmd>MarkdownPreviewStop<cr>", desc = "Stop Markdown Preview" },
+  },
 }
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Repo containin all of my configuration 
 
 ## List to Do
-1. Fix the conflict between [zsh-autocompletions & zsh-vi-mode](asdasd)
+1. Fix the conflict between [zsh-autocompletions & zsh-vi-mode](https://github.com/marlonrichert/zsh-autocomplete/issues/192)
 
 
