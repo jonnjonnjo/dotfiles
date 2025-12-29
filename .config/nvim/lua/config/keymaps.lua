c466828544948974780f2f579ffc5ac4bf4fe237
jonnjonnjo <zhenagajo234@gmail.com>
2025-12-29 20:12:51 +0700

    fix: revert back to lazyvim. Adding plugins to vanilla nvim through lazy pacman is too fucking much hassle

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,3 @@
-vim.keymap.set("n","-","<CMD>Oil<CR>",{ desc = "Open parent directory" })
-
--- For Lazy related 
-vim.keymap.set("n", "<leader>L", ":Lazy<CR>", { desc = "Run Lazy immediately" })
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,37 +1,7 @@
 return {
-	{ "ellisonleao/gruvbox.nvim" },
-	{ "e-q/okcolors.nvim", name = "okcolors",
-  -- lazy = false,
-  -- priority = 1000,
-  -- -- name = 'kanagawa-paper',
-  -- config = function()
-  --  vim.cmd.colorscheme("okcolors")
-  -- end,
-},
-	{
-		"thesimonho/kanagawa-paper.nvim",
-  -- lazy = false,
-  -- priority = 1000,
-  -- name = 'kanagawa-paper',
-  -- config = function()
-  --  vim.cmd.colorscheme("kanagawa-paper")
-  -- end,
-	},
-	{
-  'embark-theme/vim',
-  -- lazy = false,
-  -- priority = 1000,
-  -- name = 'embark',
-  -- config = function()
-  --  vim.cmd.colorscheme("embark")
-  -- end,
-},
-	{ "rebelot/kanagawa.nvim",
-  lazy = false,
-  priority = 1000,
-  name = 'kanagawa',
-  config = function()
-   vim.cmd.colorscheme("kanagawa")
-  end,
-},
+  { "ellisonleao/gruvbox.nvim" },
+  { "e-q/okcolors.nvim", name = "okcolors" },
+  { "thesimonho/kanagawa-paper.nvim" },
+  { "embark-theme/vim" },
+  { "rebelot/kanagawa.nvim" },
 }
