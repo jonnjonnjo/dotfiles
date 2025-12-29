63039922bdc06a7c5521ee646b75711344645fc3
jonnjonnjo <zhenagajo234@gmail.com>
2025-12-29 20:12:51 +0700

    fix: revert back to lazyvim. Adding plugins to vanilla nvim through lazy pacman is too fucking much hassle

---
+++ .config/nvim-old/lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n","-","<CMD>Oil<CR>",{ desc = "Open parent directory" })
+
+-- For Lazy related 
+vim.keymap.set("n", "<leader>L", ":Lazy<CR>", { desc = "Run Lazy immediately" })
---
+++ .config/nvim-old/lua/plugins/mason.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      automatic_installation = true,
+    },
+  }
+}
