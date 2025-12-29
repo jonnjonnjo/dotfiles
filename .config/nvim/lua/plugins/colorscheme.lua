356f040c7dc14981113de627a82e59ab8b1defd4
jonnjonnjo <zhenagajo234@gmail.com>
2025-12-29 20:37:37 +0700

    feat: add brightness in ff

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,4 +4,11 @@ return {
   { "thesimonho/kanagawa-paper.nvim" },
   { "embark-theme/vim" },
   { "rebelot/kanagawa.nvim" },
+  { "savq/melange-nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-lotus",
+    },
+  },
 }
