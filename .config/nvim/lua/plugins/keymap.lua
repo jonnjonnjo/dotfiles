e5f6ec27a9eb21be03c555959c1ba8cffeba8806
jonnjonnjo <zhenagajo234@gmail.com>
2026-01-04 09:57:31 +0700

    fix: move nvim snacks into right side

--- .config/nvim/lua/plugins/keymap.lua
+++ .config/nvim/lua/plugins/keymap.lua
@@ -5,20 +5,22 @@ return {
       ["*"] = {
         keys = {
           -- Add a keymap
-          { "H", "<cmd>echo 'hello'<cr>", desc = "Say Hello" },
-          -- Change an existing keymap
-          { "K", "<cmd>echo 'custom hover'<cr>", desc = "Custom Hover" },
-          -- Disable a keymap
-          { "gd", false },
-          {
-            "<leader>cp",
-            function()
-              local template_path = vim.fn.expand("~/.config/nvim/templates/competitive-programming/template.cpp")
-              local lines = vim.fn.readfile(template_path)
-              vim.api.nvim_put(lines, "l", true, true)
-            end,
-            { desc = "Insert C++ competitive programming template" },
-          },
+          -- { "H", "<cmd>echo 'hello'<cr>", desc = "Say Hello" },
+          -- -- Change an existing keymap
+          -- { "K", "<cmd>echo 'custom hover'<cr>", desc = "Custom Hover" },
+          -- -- Disable a keymap
+          -- { "gd", false },
+          --
+          -- fix keymap for competitive programming LATER!!!!
+          -- {
+          --   "<leader>cp",
+          --   function()
+          --     local template_path = vim.fn.expand("~/.config/nvim/templates/competitive-programming/template.cpp")
+          --     local lines = vim.fn.readfile(template_path)
+          --     vim.api.nvim_put(lines, "l", true, true)
+          --   end,
+          --   { desc = "Insert C++ competitive programming template" },
+          -- },
         },
       },
     },
