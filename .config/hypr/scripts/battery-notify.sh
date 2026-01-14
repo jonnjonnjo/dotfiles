be880fdfe0c8239e071b9f3b0082f614d8892f27
jonnjonnjo <zhenagajo234@gmail.com>
2026-01-14 10:39:15 +0700

    fix: uncomment hyprpaper

--- .config/hypr/scripts/battery-notify.sh
+++ .config/hypr/scripts/battery-notify.sh
@@ -4,10 +4,10 @@ BATTERY_PATH="/sys/class/power_supply/BAT0"
 CAPACITY=$(cat "$BATTERY_PATH/capacity")
 STATUS=$(cat "$BATTERY_PATH/status")
 
-if [ "$STATUS" = "Discharding"]; then 
-  if [ "$CAPACITY" -le 5]; then 
+if [ "$STATUS" = "Discharding"]; then
+  if [ "$CAPACITY" -le 5]; then
     notify-send -u critical "Battery Critical" "Battery at ${CAPACITY}%!"
-  elif [ "$CAPACITY" -le 10] then 
+  elif [ "$CAPACITY" -le 10]; then
     notify-send -u critical "Battery Low" "Battery at ${CAPACITY}%!"
-  fi 
-fi 
+  fi
+fi
