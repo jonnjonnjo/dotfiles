a053ce17cee8fe2375c9c9c85d8cc0073f98875a
jonnjonnjo <zhenagajo234@gmail.com>
2026-03-31 14:09:12 +0700

    init df

--- home.nix
+++ home.nix
@@ -51,18 +51,22 @@
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+  # pain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    ".config/hypr"  = {
+        source = ./dotfiles/hypr;
+        recursive = true;
+      }; 
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".config/kitty"  = {
+        source = ./dotfiles/kitty;
+        recursive = true;
+      }; 
+
+    ".config/nvim"  = {
+        source = ./dotfiles/nvim;
+        recursive = true;
+      }; 
   };
 
   # Home Manager can also manage your environment variables through
